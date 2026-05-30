32f73f8ed5fe94b27f3a75fc041e81f454940568
Sujay Mandal <sujaym7047@gmail.com>
2026-05-30 17:42:04 +0530

    Initial commit

--- Control_Panel/settings.py
+++ Control_Panel/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -74,12 +75,16 @@ WSGI_APPLICATION = 'Control_Panel.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+    # ...existing code...    }
+
+
 
 
 # Password validation
