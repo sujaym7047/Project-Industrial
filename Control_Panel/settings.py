712e5c9987491caaf3d6d549ff889fbe22c17f3d
Sujay Mandal <sujaym7047@gmail.com>
2026-05-30 17:59:42 +0530

    Blood Bank Management System Django Project

--- Control_Panel/settings.py
+++ Control_Panel/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-8ec8q2x_z-&_#htmfccab1o3tsfbm%_mh!-8f$#i0_$$u+2#_z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -49,6 +49,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'Control_Panel.urls'
@@ -122,7 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
